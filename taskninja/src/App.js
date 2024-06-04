652359207b4d6ca036be712ca824eb3d88f65134
rex <rohitrex321@gmail.com>
2024-06-04 16:32:27 +0530

    SearchBar added

--- taskninja/src/App.js
+++ taskninja/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
 import './App.css';
+import SearchBarFilter from "./components/SearchBar/SearchBar";
 const App = () => {
   return (
+  <div>
     <div>
       <Navbar/>
+    </div>
+    <div>
       <h1 className="Header">Book Trusted Help for home tasks</h1>
     </div>
+    <div><SearchBarFilter/></div>
+  </div>
   );
 };
 
